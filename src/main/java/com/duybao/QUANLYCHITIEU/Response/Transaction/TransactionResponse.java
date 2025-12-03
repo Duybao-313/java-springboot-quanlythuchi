bceb9a2be8847f61017c67ffbd4ad9405bdec181
Phung Dam Duy Bao <duybaocadoi2005@gmail.com>
2025-12-04 02:51:46 +0700

    add validation eror messages

--- src/main/java/com/duybao/QUANLYCHITIEU/Response/Transaction/TransactionResponse.java
+++ src/main/java/com/duybao/QUANLYCHITIEU/Response/Transaction/TransactionResponse.java
@@ -1,5 +1,6 @@
 package com.duybao.QUANLYCHITIEU.Response.Transaction;
 
+import com.duybao.QUANLYCHITIEU.Enum.TransactionType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 public class TransactionResponse {
     private Long id;
     private BigDecimal amount;
+    private TransactionType type;
     private String description;
     private String categoryName;
     private String walletName;

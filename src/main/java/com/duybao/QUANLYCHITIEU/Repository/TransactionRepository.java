a45fa093050c9414d2a69c8002023ea71832988f
Phung Dam Duy Bao <duybaocadoi2005@gmail.com>
2025-12-04 04:27:50 +0700

    fix delete transaction when deleting wallet

--- src/main/java/com/duybao/QUANLYCHITIEU/Repository/TransactionRepository.java
+++ src/main/java/com/duybao/QUANLYCHITIEU/Repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.duybao.QUANLYCHITIEU.Repository;
 
 import com.duybao.QUANLYCHITIEU.Model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByUserId(Long userId);
+    @Query("SELECT t FROM Transaction t WHERE t.user.id = :userId AND t.wallet.id = :walletId")
+    List<Transaction> findByUserAndWallet(@Param("userId") Long userId, @Param("walletId") Long walletId);
 }
--- src/main/java/com/duybao/QUANLYCHITIEU/Service/Impl/WalletServiceImpl.java
+++ src/main/java/com/duybao/QUANLYCHITIEU/Service/Impl/WalletServiceImpl.java
@@ -3,8 +3,10 @@ package com.duybao.QUANLYCHITIEU.Service.Impl;
 import com.duybao.QUANLYCHITIEU.Exception.AppException;
 import com.duybao.QUANLYCHITIEU.Exception.ErrorCode;
 import com.duybao.QUANLYCHITIEU.Mappers.WalletMapper;
+import com.duybao.QUANLYCHITIEU.Model.Transaction;
 import com.duybao.QUANLYCHITIEU.Model.User;
 import com.duybao.QUANLYCHITIEU.Model.Wallet;
+import com.duybao.QUANLYCHITIEU.Repository.TransactionRepository;
 import com.duybao.QUANLYCHITIEU.Repository.UserRepository;
 import com.duybao.QUANLYCHITIEU.Repository.WalletRepository;
 import com.duybao.QUANLYCHITIEU.Response.Wallet.Request.WalletRequest;
@@ -22,6 +24,7 @@ public class WalletServiceImpl implements WalletService {
     private final WalletRepository walletRepository;
     private final UserRepository userRepository;
     private final WalletMapper walletMapper;
+    private  final TransactionRepository transactionRepository;
 
     public WalletResponse createWallet(Long userId, WalletRequest request) {
         User user = userRepository.findById(userId)
@@ -64,11 +67,13 @@ public class WalletServiceImpl implements WalletService {
     public void deleteWallet(Long userId, Long id) {
         Wallet wallet = walletRepository.findById(id)
                 .orElseThrow(() -> new AppException(ErrorCode.WALLET_NOT_FOUND));
-
         if (!wallet.getUser().getId().equals(userId)) {
             throw new AppException(ErrorCode.USER_NOT_AUTHORIZED);
         }
-
+        if(!transactionRepository.findByUserAndWallet(userId,id).isEmpty()){
+            List<Transaction> txs = transactionRepository.findByUserAndWallet(userId, id);
+            transactionRepository.deleteAll(txs);
+        }
         walletRepository.delete(wallet);
     }
 
